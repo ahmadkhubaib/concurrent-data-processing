6add1b0f8fb4f0b7947bc38bce8e46a87e7ec50b
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2021-12-04 23:11:12 +0500

    added scaffolding for dummy scraper with GenStage

---
+++ gen_stage_scraper/lib/gen_stage_scraper.ex
@@ -0,0 +1,18 @@
+defmodule GenStageScraper do
+  @moduledoc """
+  Documentation for `GenStageScraper`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> GenStageScraper.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ gen_stage_scraper/test/gen_stage_scraper_test.exs
@@ -0,0 +1,8 @@
+defmodule GenStageScraperTest do
+  use ExUnit.Case
+  doctest GenStageScraper
+
+  test "greets the world" do
+    assert GenStageScraper.hello() == :world
+  end
+end
