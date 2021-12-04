c0ba196e455ab295292d7146617283930a333aad
Khubaib Ahmad <khubaibahmad1994@gmail.com>
2021-12-04 16:05:22 +0500

    added query for running jobs

--- job_processor/lib/job_processor.ex
+++ job_processor/lib/job_processor.ex
@@ -1,6 +1,13 @@
 defmodule JobProcessor do
   alias JobProcessor.{JobSupervisor, JobRunner}
 
+  def running_imports() do
+    match_all = {:"$1", :"$2", :"$3"}
+    guards = [{:"==", :"$3", "import"}]
+    map_result = [%{id: :"$1", pid: :"$2", type: :"$3"}]
+    Registry.select(JobProcessor.JobRegistry,[{match_all, guards, map_result}])
+  end
+
   def start_job(args) do
     DynamicSupervisor.start_child(JobRunner, {JobSupervisor, args})
   end
